94dd887bdf7f93d887a1726dbb9b58e920b1d507
AlexVolkov <33598114+VedantAudio@users.noreply.github.com>
2018-07-25 22:28:04 +0500

    Create MyFizzBuzz.scala

---
+++ MyFizzBuzz.scala
@@ -0,0 +1,22 @@
+import scala.collection._
+
+object MyFizzBuzz extends  App{
+
+  def fizzBuzz(n: Int): List[String] = {
+    var i:Int=1
+    var newList = List[String]()
+    while (i<=n) {
+      if (i%3==0 && i%5==0) newList = "FizzBuzz" :: newList
+      else if (i%3==0) newList = "Fizz" :: newList
+      else if (i%5==0) newList = "Buzz" :: newList
+      else newList = i.toString :: newList
+
+
+      i+=1
+    }
+
+    newList.reverse
+  }
+
+  println(fizzBuzz(30))
+}
