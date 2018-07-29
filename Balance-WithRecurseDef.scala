69d0be54bb757148536f1e39cb5284904789b2ec
AlexVolkov <33598114+VedantAudio@users.noreply.github.com>
2018-07-29 18:14:25 +0500

    Create Balance-WithRecurseDef.scala

---
+++ Balance-WithRecurseDef.scala
@@ -0,0 +1,23 @@
+object Parents extends App {
+
+  def balance(chars: List[Char]): Boolean = {
+    var myBalance: Int = 0
+
+    def myBalanceInto(chars: List[Char]): Boolean = {
+      if (chars.isEmpty) {
+        if (myBalance != 0) false
+        else true
+      }
+      else if (chars.head == ')' && (myBalance == 0)) false
+      else {
+        if (chars.head == '(') myBalance += 1
+        if (chars.head == ')') myBalance -= 1
+        myBalanceInto(chars.tail)
+      }
+    }
+
+    myBalanceInto(chars)
+  }
+
+  println(balance("(gjfgjgfjgfj)".toList), balance("())(".toList), balance("(dfsfs(ff(sef(fdfd)fsfff)))".toList))
+}
