c4573672a8ed85041e99926180475a9ec2ff894d
AlexVolkov <33598114+VedantAudio@users.noreply.github.com>
2018-07-30 00:12:53 +0500

    Create Palindrome.scala

---
+++ Palindrome.scala
@@ -0,0 +1,21 @@
+object SolutionsPalindrome extends App {
+  def isPalindrome(x: Int): Boolean = {
+    if (x >= 0) {
+      var xNumberChar: List[Char] = List(x).flatMap(_.toString.toList)
+      var xNumberCharSize: Int = xNumberChar.size
+      var xReversNumber: Int = 0
+      var decMultiply: Int = 1
+      while (xNumberChar.nonEmpty) {
+        xReversNumber += xNumberChar.head.asDigit * decMultiply
+        decMultiply *= 10
+        xNumberChar = xNumberChar.tail
+      }
+      if (x == xReversNumber) true
+      else false
+    }
+    else false
+  }
+
+  println(isPalindrome(1334331), "=13340001\n", isPalindrome(100031), "=100031\n", 
+  isPalindrome(-131), "=-1331\n", isPalindrome(133331), "=133331")
+}
