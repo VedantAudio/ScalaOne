23d2ed8868648b41e8da54cb0855f5d1b3e42a26
AlexVolkov <33598114+VedantAudio@users.noreply.github.com>
2018-07-29 14:57:34 +0500

    Create Balance().scala

---
+++ Balance().scala
@@ -0,0 +1,19 @@
+object Parents extends App {
+  var myBalance: Int = 0
+  
+  def balance(chars: List[Char]): Boolean = {
+
+    if (chars.isEmpty) {
+      if (myBalance != 0) false
+      else true
+    }
+    else if (chars.head == ')' && (myBalance == 0)) false
+    else {
+      if (chars.head == '(') myBalance += 1
+      if (chars.head == ')') myBalance -= 1
+      balance(chars.tail)
+    }
+  }
+
+  println(balance("(gjfgjgfjgfj)".toList), balance("())(".toList), balance("(dfsfs(ff(sef(fdfd)fsfff".toList))
+}
