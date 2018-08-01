f5bfe2bbafa0af0972988df901d2ca5a49fd4594
AlexVolkov <33598114+VedantAudio@users.noreply.github.com>
2018-08-01 20:34:45 +0500

    Update CheckNewName2.scala

--- CheckNewName2.scala
+++ CheckNewName2.scala
@@ -1,7 +1,7 @@
 object MyDataBase extends App {
-var ii:Int= 0
+  var ii: Int = 0
   var myMap = Map(0 -> "Alex", 1 -> "Alex-1", 2 -> "Maria", 3 -> "Maria-1", 4 -> "Petia")
-var usedName:Int = 0
+  var usedName: Int = 0
 
   def checkNewName(newName: String): Boolean = {
     var i: Int = 0
@@ -13,34 +13,28 @@ var usedName:Int = 0
   }
 
   def putNewName(newName: String): String = {
-
-    var newNameCheck:String = ""
+    var newNameCheck: String = ""
     var i: Int = 0
     while (i <= myMap.size) {
       if (myMap.get(i) == Option(newName)) {
-        /**newNameCheck=newName+"-new"
-        myMap=myMap + ((myMap.size + 1) -> newNameCheck)*/
+        /** newNameCheck=newName+"-new"
+          * myMap=myMap + ((myMap.size + 1) -> newNameCheck) */
 
-        return  "This name uzed"
+        return "This name uzed"
       }
-
       i += 1
     }
     myMap = myMap + ((myMap.size + 1) -> newName)
-    usedName=1
-"You new Name is "+newName
-
+    usedName = 1
+    "You new Name is " + newName
   }
-  while (ii<1) {
-  println("Enter your name")
+
+  while (ii < 1) {
+    println("Enter your name")
 
     val enterName = scala.io.StdIn.readLine()
     println(putNewName(enterName))
-    if (usedName==1) ii=1
+    if (usedName == 1) ii = 1
     else println("Please input another name. For example: ")
-
   }
-
-
-
 }
